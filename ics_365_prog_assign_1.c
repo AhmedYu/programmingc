e469c33ec275a1a2744b42a74bf0ad5644fd003f
AhmedYu <abyan-adan@hotmail.com>
2023-05-29 18:40:22 -0500

    first function is completed 2 more functions to go

--- ics_365_prog_assign_1.c
+++ ics_365_prog_assign_1.c
@@ -6,9 +6,10 @@
 *   
 */
 #include<stdio.h>
-#include<string.h>
+ #include<string.h>
+ #include<stdlib.h>
 // Function declaration
-// void digitSeparater(int numbers);
+void digitSeparater(int numbers);
 int main(){
    // Function declaration
   int checkAnswerOfNumber(int minValue, int maxValue) ;
@@ -16,8 +17,10 @@ int main(){
     
    //TODO: if the num is less than 1 or greater than 32767 ask agian 
 
- printf("You entered , %i",  num);
-//  digitSeparater(32767);
+ printf("You entered  %i\n",  num);
+
+ digitSeparater(num);
+  
 }
 /********************************************************************
  * The function ensures the input fals in between minValue and maxValue 
@@ -41,29 +44,33 @@ int checkAnswerOfNumber(int minValue, int maxValue) {
         if (strcmp(&answer, "y") == 0 || strcmp(&answer, "Y") == 0) {
             printf("Now enter the number: ");
             scanf("%i", &num);
+            //loop until th number is in between minvalue and maxvalue
             while (num < minValue || num > maxValue) {
                 printf("Invalid number. Please enter a number between %d and %d: ", minValue, maxValue);
-                scanf("%i", &num);
+                scanf("%i", &num); //accept input from user and store it in num variable.
             }
         }
     }
     
-    return num;
+    return num; 
 }
   
 /*
 this function will accept an integer from 1 to 32767
 it will print a series of digits with two spaces separating each digit
 */
-// void digitSeparater(int numbers){
- 
-//     if(numbers <= 32767){
-// char numbersArray[] = {numbers};
-//  printf("%s",  * numbersArray);
-//  for(int i = 0 ; i < 5; i++){
-// // printf("%c",  numbersArray[i]  );
-// // printf("\t");
-//  }
-//     }
+ void digitSeparater(int numbers) {
+    char strNumbers[6]; // Allocate a character array to store the string (including null terminator)
     
-// }
+    sprintf(strNumbers, "%d", numbers); // Convert the numbers into a string
+     
+    //using null terminator check the end of string and stop 
+    // if you come to it or if the string siz is not greater than i.
+
+    for (int i = 0; i < sizeof(strNumbers) && strNumbers[i] != '\0'; i++) {
+        printf("%c  ", strNumbers[i]); //print the each with two spaces a head of it
+    }
+    
+    printf("\n");
+}
+
