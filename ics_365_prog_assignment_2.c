dfbc6a3d895c17bdb26f41143f09ea17ccfebe7c
AhmedYu <abyan-adan@hotmail.com>
2023-06-05 20:00:01 -0500

    Assignment2 intial code

---
+++ ics_365_prog_assignment_2.c
@@ -0,0 +1,13 @@
+/*
+* Class: ICS 365-50 SUMMAR 2023
+* Assignment: Programming Assignment 2
+* File name: ics_365_prog_assign_2.c
+* Author: AHMED YUSSUF
+*/
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("testing");
+    /* code */
+    return 0;
+}
