1326e7ca3a5ee59f911cace24c2040c13dd6e993
AhmedYu <abyan-adan@hotmail.com>
2023-06-22 16:06:38 -0500

    deleted every thing

--- ics_365_prog_assignment_2.c
+++ ics_365_prog_assignment_2.c
@@ -4,53 +4,10 @@
 * File name: ics_365_prog_assignment_2.c
 * Author: AHMED YUSSUF
 */
-#include<stdio.h>
- //function prototype
-unsigned int getInput(); 
-//function prototype
-unsigned int*  convertToBinary(unsigned int number);
- //function prototype
-// void printBinaryArray(unsigned int* binaryArray);
- //function prototype
-// void reverseBinary(unsigned int* binaryArray, int size);
-
-int main() {
-    unsigned int number = getInput();
-  
- 
-
-    printf("Before bits are reversed:\n");
-    printf("\t%u = ", number);
-     
-  
-
-   
-
+ #include<stdio.h>
+ int main(int argc, char const *argv[])
+ {
+    /* code */
     return 0;
-}
-
-unsigned int getInput() {
-    unsigned int num;
-    printf("Enter unsigned integer number: ");
-    scanf("%u", &num);
-    return num;
-}
-
-unsigned int *  convertToBinary(unsigned int number) {
-    static  unsigned int binaryArray[32];
-
-    for (int i = 31; i >= 0; i--) {
-        int bit = (number >> i) & 1;
-     printf("%u", bit);
-    }
-return binaryArray;
-    
-}
-
+ }
  
-  void printBinaryArray(unsigned int* binaryArray) {
-    for (int i = 31; i >= 0; i--) {
-        printf("%u", binaryArray[i]);
-    }
-    printf("\n");
-}
